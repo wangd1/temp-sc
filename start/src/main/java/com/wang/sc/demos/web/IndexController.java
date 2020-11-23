2f14b2f2907300f27d0e175606ab1912c1eda1d7
wangd1 <wangdi1208i@gmail.com>
2020-11-23 15:43:46 +0800

    clear 222

--- start/src/main/java/com/wang/sc/demos/web/IndexController.java
+++ start/src/main/java/com/wang/sc/demos/web/IndexController.java
@@ -29,7 +29,7 @@ public class IndexController {
     private ArrayList<User> queryUsers() {
         ArrayList<User> users = new ArrayList<>();
         for (int i = 0; i < 1000; i++) {
-            users.add(new User(i,"zhuge222"));
+            users.add(new User(i,"zhuge"));
         }
         return users;
     }
